d56effab1850dc4885e4bf90073c48b517ae1133
AldairOrellana <64727634+AldariOrellana@users.noreply.github.com>
2026-09-18 03:30:43 -0600

    approuter temp

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,73 +1,28 @@
 import { Routes } from '@angular/router';
-
 import { AppLayout } from './app/layout/component/app.layout';
-
+import { Dashboard } from './app/pages/dashboard/dashboard';
 import { Login } from './app/pages/auth/login';
-
-import {
-    RegistroPage
-} from './app/pages/registro/registro';
-
-import {
-    Landing
-} from './app/pages/landing/landing';
-
-import {
-    Dashboard
-} from './app/pages/dashboard/dashboard';
-
-import {
-    NotificacionesPage
-} from './app/pages/notificaciones/notificaciones';
-
-import {
-    autenticacionGuard
-} from './app/core/guards/autorizacion.guard';
+import { RegistroPage } from './app/pages/registro/registro';
+import { ReportesPage } from './app/pages/reportes/reportes';
+import { TecnicosPage } from './app/pages/tecnicos/tecnicos';
+import { Landing } from './app/pages/landing/landing';
+import { NotificacionesPage } from './app/pages/notificaciones/notificaciones';
+import { autenticacionGuard, rolGuard } from './app/core/guards/autorizacion.guard';
 
 export const appRoutes: Routes = [
-    {
-        path: '',
-        component: Landing,
-        pathMatch: 'full',
-        title: 'ApagónYa'
-    },
+    { path: '', component: Landing, pathMatch: 'full', title: 'ApagónYa' },
     {
         path: '',
         component: AppLayout,
-        canActivate: [
-            autenticacionGuard
-        ],
+        canActivate: [autenticacionGuard],
         children: [
-            {
-                path: 'panel',
-                component: Dashboard,
-                title:
-                    'Panel | ApagónYa'
-            },
-            {
-                path: 'notificaciones',
-                component:
-                    NotificacionesPage,
-                title:
-                    'Notificaciones | ApagónYa'
-            }
+            { path: 'panel', component: Dashboard, title: 'Panel | ApagónYa' },
+            { path: 'tecnicos', component: TecnicosPage, canActivate: [rolGuard], data: { roles: ['ADMIN'] }, title: 'Técnicos | ApagónYa' },
+            { path: 'reportes', component: ReportesPage, title: 'Reportes | ApagónYa' },
+            { path: 'notificaciones', component: NotificacionesPage, title: 'Notificaciones | ApagónYa' }
         ]
     },
-    {
-        path: 'auth/login',
-        component: Login,
-        title:
-            'Iniciar sesión | ApagónYa'
-    },
-    {
-        path: 'registro',
-        component:
-            RegistroPage,
-        title:
-            'Crear cuenta | ApagónYa'
-    },
-    {
-        path: '**',
-        redirectTo: ''
-    }
+    { path: 'auth/login', component: Login, title: 'Iniciar sesión | ApagónYa' },
+    { path: 'registro', component: RegistroPage, title: 'Crear cuenta | ApagónYa' },
+    { path: '**', redirectTo: '' }
 ];
